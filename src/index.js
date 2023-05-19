653137c9d272705f26858cb454dec9391efaeb3e
Djordje Gavrilovic <gavrilovicmdj@gmail.com>
2023-05-19 12:37:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log("app loaded");
+
+function initF() {
+  console.log("initF...");
+  let dseObjectId = window.dseObjectConfig.dseObject.instanceId;
+  console.log("dse object id: " + dseObjectId);
+
+  let dseObjectId_2 = window.parent.dseObjectConfig.dseObject.instanceId;
+  console.log("dse object id 2: " + dseObjectId_2);
+
+  let selectedAssets = window.parent.document.querySelectorAll('.asset-selected');
+  let selectedIds = [];
+  for (let i = 0; i < selectedAssets.length; i++) {
+    selectedIds.push(selectedAssets[i].id);
+  }
+  console.log("selected ids: " + selectedIds);
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
